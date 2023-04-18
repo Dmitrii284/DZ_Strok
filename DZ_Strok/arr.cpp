5cc58f6990eab488c3327710212c37b0befcc4e9
Б - 12 < - 12@DESKTOP-BM1VSFR>
2023-04-18 19:05:56 +0300

    22

--- DZ_Strok/arr.cpp
+++ DZ_Strok/arr.cpp
@@ -25,36 +25,53 @@ int isThinks(std::string str) {
 			count++;
 		return count;	
 	}
+//Task 4
+std::string Substr(std::string &Str,char sym) {
+	//std::string ret_Str;	
+	//if (Str.rfind(sym))
+		return	Str.substr(sym);
+}
+
+
+
 
 int main() {
 	setlocale(LC_ALL, "Rus");
 
 	int n = 0;
 
-	//Task 1
-	std::string str = "Hello world!";
-	char sym ='o';	
-	std::string res;
-	for (int i = 0; i < str.length(); i++)
-		if (str[i] == sym)
-			res += sym;
-	std::cout << res << std::endl;
+	////Task 1
+	//std::string str = "Hello world!";
+	//char sym ='o';	
+	//std::string res;
+	//for (int i = 0; i < str.length(); i++)
+	//	if (str[i] == sym)
+	//		res += sym;
+	//std::cout << res << std::endl;
 
 	//Task 2	
-	std::string word_str = std::to_string(n);
+	/*std::string word_str = std::to_string(n);
 	std::cout << "Enter the word->";
 	std::getline(std::cin, word_str);
 	for (int i = 0; i < word_str.length(); i++)
 		word_str[i] = std::tolower(word_str[i]);
 	std::cout << isPalidrome(word_str);
-	std::cout << std::endl;
+	std::cout << std::endl;*/
 
 	//Task 3
-	std::string w_str = std::to_string(n);
+	/*std::string w_str = std::to_string(n);
 	std::cout << "Enter the word->";
 	std::getline(std::cin, w_str);	
-	std::cout << isThinks(w_str);
+	std::cout << isThinks(w_str);*/
 
-	
+	//Task 4
+	std::string text_str = std::to_string(n);
+	std::cout << "Enter the text-> ";
+	std::getline(std::cin, text_str);
+	char sym;
+	std::cout << "Enter the symbol-> ";
+	std::cin >> sym;
+	std::cout << Substr(text_str,sym);
+
 	return 0;
 }
